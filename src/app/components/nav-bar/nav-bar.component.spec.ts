0becc1d1c482595969cd99c111bcf2ec0b027708
SerenaGiumentaro <serena.giumentaro@gmail.com>
2023-03-23 19:10:40 +0100

    nav bar bug fix

--- src/app/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,10 +1,19 @@
+import { DialogRef } from '@angular/cdk/dialog';
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { By } from '@angular/platform-browser';
 import { provideRouter, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { LoginService } from 'src/app/login-signup/services/login.service';
+import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
+import { DialogService } from 'src/app/shared/services/dialog.service';
 import { NavBarComponent } from './nav-bar.component';
 
 describe('NavBarComponent', () => {
@@ -12,12 +21,29 @@ describe('NavBarComponent', () => {
   let fixture: ComponentFixture<NavBarComponent>;
   let router: Router;
   let navigateSpy: jasmine.Spy;
-  let navigateSpyByUrl: jasmine.Spy
+  let navigateSpyByUrl: jasmine.Spy;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let dialogService: DialogService;
+  let loginService: LoginService;
+  let mockDialog: jasmine.SpyObj<MatDialog>
+  let drawDialogSpy : jasmine.Spy
   beforeEach(async () => {
+    mockDialog = jasmine.createSpyObj(['open']);
+    drawDialogSpy = spyOn(DialogService.prototype, 'drawDialog')
     await TestBed.configureTestingModule({
       declarations: [NavBarComponent],
-      imports: [RouterTestingModule, MatToolbarModule, MatIconModule],
+      imports: [
+        RouterTestingModule,
+        MatToolbarModule,
+        MatIconModule,
+        HttpClientTestingModule,
+        MatDialogModule,
+      ],
       providers: [
+        DialogService,
+        LoginService,
+        { provide: MatDialog, useValue: mockDialog },
         provideRouter([
           {
             path: 'posts',
@@ -30,10 +56,14 @@ describe('NavBarComponent', () => {
 
     fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
-    router = TestBed.inject(Router)
-    navigateSpy = spyOn(router, 'navigate')
-    navigateSpyByUrl = spyOn(router, 'navigateByUrl')
-    navigateSpy.calls.reset
+    router = TestBed.inject(Router);
+    navigateSpy = spyOn(router, 'navigate');
+    navigateSpyByUrl = spyOn(router, 'navigateByUrl');
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    dialogService = TestBed.inject(DialogService);
+    loginService = TestBed.inject(LoginService);
+    navigateSpy.calls.reset;
     fixture.detectChanges();
   });
 
@@ -51,13 +81,13 @@ describe('NavBarComponent', () => {
     );
   });
 
-  it(`should navigate to dashboard`, () => {
+  it(`should navigate to users`, () => {
     const button = fixture.debugElement.query(
-      By.css('button[routerLink="dashboard"')
+      By.css('button[routerLink="users"')
     );
     button.nativeElement.click();
     expect(router.navigateByUrl).toHaveBeenCalledWith(
-      router.createUrlTree(['dashboard']),
+      router.createUrlTree(['users']),
       jasmine.anything()
     );
   });
@@ -72,23 +102,25 @@ describe('NavBarComponent', () => {
       jasmine.anything()
     );
   });
+  describe(`Logout`, () => {
+    beforeEach(() => {
+      const button = fixture.debugElement.query(By.css('button:last-child'));
+      spyOn(component, 'logout');
+      button.triggerEventHandler('click', null);
+    });
+    it(`should call the logout when clicked`, () => {
+      expect(component.logout).toHaveBeenCalled();
+    });
 
-  it(`should call logout function when clicked`, () => {
-    const button = fixture.debugElement.query(By.css('button:last-child'));
-    spyOn(component, 'logout');
-    button.triggerEventHandler('click', null);
-    expect(component.logout).toHaveBeenCalled();
-  });
+    it(`should open dialog calling dialog service`, () => {
+      const mockDialogConfig = {
+        title: 'Logout',
+        body: `Vuoi uscire dall'appliocazione?`,
+        isDenialNeeded: true,
+      };
+      component.logout();
 
-  it('should clear the local storage after logout', () => {
-    spyOn(localStorage, 'clear');
-    component.logout();
-    expect(localStorage.length).toBe(0);
-    expect(localStorage.clear).toHaveBeenCalled();
-  });
 
-  it('should navigate to login component after logout', () => {
-    component.logout();
-    expect(router.navigate).toHaveBeenCalledWith(['login']);
+    });
   });
 });
