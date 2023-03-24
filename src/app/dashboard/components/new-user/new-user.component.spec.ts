1227b2b3001488cfc8d53a742105876a4ee33664
SerenaGiumentaro <serena.giumentaro@gmail.com>
2023-03-24 20:03:04 +0100

    nav bar bug fix and test

--- src/app/dashboard/components/new-user/new-user.component.spec.ts
+++ src/app/dashboard/components/new-user/new-user.component.spec.ts
@@ -1,16 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NewUserComponent } from './new-user.component';
 
 describe('NewUserComponent', () => {
   let component: NewUserComponent;
   let fixture: ComponentFixture<NewUserComponent>;
+  let mockDialog: jasmine.SpyObj<MatDialog>;
+  let mockDialogRef: jasmine.SpyObj<MatDialog>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NewUserComponent ]
-    })
-    .compileComponents();
+      declarations: [NewUserComponent],
+      imports: [
+        HttpClientTestingModule,
+        MatDialogModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        { provide: MatDialog, useValue: mockDialog },
+        { provide: MatDialogRef, useValue: mockDialogRef },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NewUserComponent);
     component = fixture.componentInstance;
--- src/app/dashboard/components/user-card/user-card.component.spec.ts
+++ src/app/dashboard/components/user-card/user-card.component.spec.ts
@@ -1,23 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { User } from 'src/app/interface';
 import { UserCardComponent } from './user-card.component';
 
 describe('UserCardComponent', () => {
   let component: UserCardComponent;
   let fixture: ComponentFixture<UserCardComponent>;
+  const mockUser: User = {
+    name: 'Test User',
+    email: 'user@test.com',
+    id: 111,
+    status: 'active',
+    gender: 'female',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserCardComponent ]
-    })
-    .compileComponents();
+      declarations: [UserCardComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserCardComponent);
     component = fixture.componentInstance;
+    component.user = mockUser
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should receive the user from the id user', () => {
+    expect(component.user).toEqual(mockUser);
+    expect(component.user.id).toEqual(mockUser.id);
+  });
+  it('should render the correct gender icon', () => {
+    const genderIcon = fixture.debugElement.query(By.css('#icon'));
+    expect(genderIcon.nativeElement.innerText).toEqual('face_3');
+  });
+  it('should render the correct user name', () => {
+    const title = fixture.debugElement.query(By.css('h1'));
+    expect(title.nativeElement.innerText).toEqual('Test User');
+  });
+  it('should render the correct user email', () => {
+    const title = fixture.debugElement.query(By.css('h3'));
+    expect(title.nativeElement.innerText).toEqual('user@test.com');
   });
 });
